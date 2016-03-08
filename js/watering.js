00a45b3364edaf734583a1743bf5567b716c7f41
Paul-Hess <pauljacksonhess@gmail.com>
2016-03-07 22:51:40 -0800

    refine jquery func to submit form as one

--- js/watering.js
+++ js/watering.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
+	var repeatRemind = $('#select-all');
+	repeatRemind.change(function() {
+		if ($(this).is(':checked') ) {
 
+			$('.day').not('.monday').css('display', 'none');
+			var hour = $('.monday .select-group .hours').val();
+			var mins = $('.monday .select-group .minutes').val();
+			var meridian = $('.monday .select-group .meridian').val();
+			
+			$('.hours').not('.monday .select-group .hours').each(function() {
+				$(this).val(hour);
+			});
+
+			$('.minutes').not('.monday .select-group .minutes').each(function() {
+				$(this).val(mins);
+			});
+
+			$('.meridian').not('.monday .select-group .meridian').each(function() {
+				$(this).val(meridian);
+			});
+
+		}
+	});
+		
 });
