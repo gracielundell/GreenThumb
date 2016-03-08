fde4e68dbba333ec19aea052065e8b7dd0c07099
Paul-Hess <pauljacksonhess@gmail.com>
2016-03-08 09:30:42 -0800

    restyle for form after js animates page

--- js/watering.js
+++ js/watering.js
@@ -3,7 +3,14 @@ $(document).ready(function() {
 	repeatRemind.change(function() {
 		if ($(this).is(':checked') ) {
 
-			$('.day').not('.monday').css('display', 'none');
+			$(this).addClass('visually-hidden');
+			$('label[for="select-all"').addClass('hidden');
+			$('.day').not('.monday').addClass('hidden visually-hidden');
+			$('.monday').addClass('prioritize');
+			$('.time-select').addClass('reorder');
+			$('input[type="text"]').addClass('priority-two');
+			$('button[type="submit"').addClass('priority-three');
+
 			var hour = $('.monday .select-group .hours').val();
 			var mins = $('.monday .select-group .minutes').val();
 			var meridian = $('.monday .select-group .meridian').val();
